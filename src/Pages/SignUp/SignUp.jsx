b9b1c6d5a7b76d469fc541ce3eb0c29d755336b0
Sojib Das <sojibdas1015@gmail.com>
2026-04-04 00:32:19 +0600

    register email and password

--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -1,18 +1,62 @@
-import React from 'react';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import React, { useState } from 'react';
+import { auth } from '../../Firebase/firebase.init';
+import { FaEye } from "react-icons/fa";
+
 
 const SignUp = () => {
+    const [errorMassage, setErrorMassage] = useState('');
+    const [success, setSuccess] = useState(false)
+    const handleForm = (event) => {
+        event.preventDefault()
+        const email = event.target.email.value;
+        const password = event.target.password.value
+        setSuccess(false)
+        console.log(email, password)
+        setErrorMassage('')
+        createUserWithEmailAndPassword(auth, email, password)
+            .then(result => {
+                console.log(result)
+                setSuccess(true)
+            })
+            .catch(error => {
+                console.log(error)
+                setErrorMassage(error.message)
+            })
+    }
+
     return (
 
         <div className="card bg-base-100 w-full mx-auto max-w-sm shrink-0 shadow-2xl mt-10">
             <div className="card-body">
-            <h1 className="text-5xl font-bold">Register Now</h1>
-                <fieldset className="fieldset">
+                <h1 className="text-5xl font-bold">Register Now</h1>
+                <form onSubmit={handleForm} className="fieldset">
                     <label className="label">Email</label>
-                    <input type="email" className="input" placeholder="Email" />
-                    <label className="label">Password</label>
-                    <input type="password" className="input" placeholder="Password" />
+                    <input type="email" name='email' className="input" placeholder="Email" />
+                    <label className="input validator">
+                        
+                        <input
+                            type="password"
+                            name='password'
+                            required
+                            placeholder="Password"
+                            minLength="8"
+                            pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+                            title="Must be more than 8 characters, including number, lowercase letter, uppercase letter"
+                        />
+                        <FaEye />
+
+                    </label>
+                    <p className="validator-hint hidden">
+                        Must be more than 8 characters, including
+                        <br />At least one number <br />At least one lowercase letter <br />At least one uppercase letter
+                    </p>
                     <button className="btn btn-neutral mt-4">Register</button>
-                </fieldset>
+                </form>
+                <p className='text-red-700'>{errorMassage}</p>
+                {
+                    success && <p className='text-green-700'>Successfully Registered</p>
+                }
             </div>
         </div>
     );
