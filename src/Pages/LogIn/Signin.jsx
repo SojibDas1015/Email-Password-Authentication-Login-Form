f8179f6ebc3b561452653d841851892eadf57128
Sojib Das <sojibdas1015@gmail.com>
2026-04-07 20:10:49 +0600

    reset password link send email and reset password

--- src/Pages/LogIn/Signin.jsx
+++ src/Pages/LogIn/Signin.jsx
@@ -1,11 +1,12 @@
 
-import { signInWithEmailAndPassword } from 'firebase/auth';
+import { sendEmailVerification, sendPasswordResetEmail, signInWithEmailAndPassword } from 'firebase/auth';
 import React, { useRef, useState } from 'react';
 import { auth } from '../../Firebase/firebase.init';
 
 const Signin = () => {
     const [errorMassage, setErrorMassage] = useState('');
     const [success, setSuccess] = useState(false)
+    const emailref = useRef();
     const handleFormLogin = (event) => {
         event.preventDefault()
         const email = event.target.email.value;
@@ -30,21 +31,32 @@ const Signin = () => {
             setErrorMassage(error.message)
         })
     }
+    const hanldeForgetPassword =() => {
+        const emailAddress = emailref.current.value;
+        sendPasswordResetEmail(auth, emailAddress)
+        .then(result => {
+            console.log(result);
+            alert('Password Reset Link Send Your Email')
+        })
+        .catch(error => {
+            console.log(error)
+        })
+    }
     return (
         <div className="card bg-base-100 w-full mx-auto max-w-sm shrink-0 shadow-2xl mt-10">
             <h1 className='text-4xl font-bold text-center'>Login</h1>
             <div className="card-body">
                 <form onSubmit={handleFormLogin} className="fieldset">
                     <label className="label">Email</label>
-                    <input type="email" name='email' className="input" placeholder="Email" />
+                    <input type="email" ref={emailref} name='email' className="input" placeholder="Email" />
                     <label className="label">Password</label>
                     <input type="password" name='password' className="input" placeholder="Password" />
 
-                    <div><a className="link link-hover">Forgot password?</a></div>
+                    <div><a onClick={hanldeForgetPassword} className="link link-hover">Forgot password?</a></div>
                     <button className="btn btn-neutral mt-4">Login</button>
                 </form>
                 <p className='text-red-800'>{errorMassage}</p>
-                <p className='text-green-700'>{success}</p>
+                <p className='text-green-700'>{success && 'Success'}</p>
             </div>
         </div>
     );
